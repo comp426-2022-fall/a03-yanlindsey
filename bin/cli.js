4c7e67a64edd971b117dcb9eab59b1cfca2c233d
yanlindsey <ylinds@live.unc.edu>
2022-11-28 10:01:16 -0500

    added type to package

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import minimist from "minimist";
-import { roll } from "/lib/roll.js";
+import { roll } from "../lib/roll.js";
 
 const args = minimist(process.argv.slice(2));
 
@@ -9,4 +9,4 @@ const side = args.side || 6;
 const dice = args.dice || 2;
 const rolls = args.rolls || 1;
 
-console.log(roll(argv["side"], argv["dice"], argv["rolls"]));
+console.log(JSON.stringify(roll(side, dice, rolls)));
